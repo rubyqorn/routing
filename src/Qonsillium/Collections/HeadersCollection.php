45dc9ef9a3e98390269f88d98a8dfe9f09a1579f
rubyqorn <antonhideger1337@gmail.com>
2020-11-14 19:10:23 +0300

    Created attach and detach collection method in file, headers, cookie managers

--- src/Qonsillium/Collections/HeadersCollection.php
+++ src/Qonsillium/Collections/HeadersCollection.php
@@ -5,11 +5,57 @@ namespace Qonsillium\Collections;
 class HeadersCollection extends Collection
 {
     /**
+     * Initiate HeadersCollection constructor method and
+     * flip array with parameters 
+     * @param array $parametersCollection
+     * @return void 
+     */ 
+    public function __construct(array $parametersCollection)
+    {
+        $parameters = array_combine(
+            $parametersCollection, $parametersCollection
+        );
+
+        parent::__construct($parameters);
+    }
+
+    /**
      * Return collection list with HTTP headers
      * @return \Qonsillium\Collections\CollectionUnitList 
      */ 
     public function getCollection(): CollectionUnitList
     {
         return $this->collectionList;
+    }
+
+    /**
+     * Set new HTTP header in headers list 
+     * @param string $headerName    | Content-type
+     * @param string $headerValue   | plain/text
+     * @return bool
+     */ 
+    public function attachHeaderCollection(string $headerName, string $headerValue)
+    {
+        $headerName = "{$headerName}: $headerValue";
+        header($headerName);
+
+        $this->collectionList->addCollectionUnit(
+            new CollectionUnit($headerName, $headerValue)
+        );
+
+        return true;
+    }
+
+    /**
+     * Remove HTTP header from headers list 
+     * @param string $headerName | Content-type: plain/text
+     * @return bool 
+     */ 
+    public function detachHeaderCollection(string $headerName)
+    {
+        header_remove($headerName);
+        $this->collectionList->removeCollectionUnit($headerName);
+
+        return true;
     }
 }
